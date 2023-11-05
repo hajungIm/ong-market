6efd79c60b53285b6e4c7a5b20e202223a7e0207
angelaoh0619 <121957335+angelaoh0619@users.noreply.github.com>
2023-11-05 10:21:55 +0900

    Update application.py

--- application.py
+++ application.py
@@ -6,53 +6,31 @@ application = Flask(__name__)
 def hello():
     return render_template("index.html")
 
-@application.route("/find_id")
+@application.route("/list")
 def view_list():
-    return render_template("find_id.html")
+    return render_template("list.html")
 
-@application.route("/find_id_success")
-def view_list():
-    return render_template("find_id_success.html")
+@application.route("/reg_item")
+def reg_item():
+    return render_template("reg_item.html")
 
-@application.route("/find_id_fail")
-def view_list():
-    return render_template("find_id_fail.html")
+@application.route("/submit_item")
+def reg_item_submit():
+    userId=request.args.get("userId")
+    itemName=request.args.get("itemName")
+    price=price.args.get("price")
+    status=status.args.get("status")
+    description=status.args.get("description")
+    transaction=status.args.get("transaction")
+    location=status.args.get("location")
+    
+    print(userId, itemName, price, status, description, transaction, location)
+    return render_template("")
 
-@application.route("/find_password")
-def view_list():
-    return render_template("find_password.html")
-
-@application.route("/find_password_success")
-def view_list():
-    return render_template("find_password_success.html")
-
-@application.route("/find_password_fail")
-def view_list():
-    return render_template("find_password_fail.html")
-
-@application.route("/item_detail")
-def view_list():
-    return render_template("item_detail.html")
-
-@application.route("/login")
-def view_list():
-    return render_template("login.html")
-
-@application.route("/mem_register")
-def view_list():
-    return render_template("mem_register.html")
-
-@application.route("/review_detail")
-def view_list():
-    return render_template("review_detail.html")
-
-@application.route("/student_check")
-def view_list():
-    return render_template("student_check.html")
-
-@application.route("/student_check_fail")
-def view_list():
-    return render_template("student_check_fail.html")
+@application.route("/submit_item_post", methods=['POST'])
+def reg_item_submit_post():
+    data=request.form
+    return render_template("result.html", data=data)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
