ecc38f8ee53ba67b06dea7c2b1be3f874584c181
angelaoh0619 <121957335+angelaoh0619@users.noreply.github.com>
2023-11-06 15:24:45 +0900

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return render_template("index.html")
+    return render_template("first_page.html")
 
 @application.route("/find_id")
 def find_id():
