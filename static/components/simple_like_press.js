8db857ee2206d31df4615f70e83b0c5ed3bb2f33
rnrgll <shuajjjjj@naver.com>
2023-11-07 02:10:32 +0900

    상품 상세보기 화면 디자인 및 레이아웃 수정

---
+++ static/components/simple_like_press.js
@@ -0,0 +1,20 @@
+// 클래스 이름으로 모든 요소를 가져옴
+const likeElements = document.querySelectorAll(".item_box1_like");
+
+// 모든 요소에 대해 클릭 이벤트 리스너를 추가
+likeElements.forEach(function (likeElement) {
+  const bookmarkIcon = likeElement.querySelector(".fa-bookmark");
+
+  bookmarkIcon.addEventListener("click", function () {
+    // 현재 클래스를 확인하고 클래스를 변경
+    if (bookmarkIcon.classList.contains("fa-regular")) {
+      bookmarkIcon.classList.remove("fa-regular");
+      bookmarkIcon.classList.add("fa-solid");
+      bookmarkIcon.style.color = "#242424";
+    } else {
+      bookmarkIcon.classList.remove("fa-solid");
+      bookmarkIcon.classList.add("fa-regular");
+      bookmarkIcon.style.color = "#606060";
+    }
+  });
+});
