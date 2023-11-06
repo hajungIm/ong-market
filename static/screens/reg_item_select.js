c5d65f361ce54a71fad7ba312f9abd73699c3193
rnrgll <shuajjjjj@naver.com>
2023-11-07 05:13:35 +0900

    상품 등록 거래방식 라디오 버튼 커스텀

--- static/screens/reg_item_select.js
+++ static/screens/reg_item_select.js
@@ -1,7 +1,6 @@
 //거래방식 라디오 버튼 요소 가져오기
-const f2fRadio = document.getElementById("f2f");
-const noF2fRadio = document.getElementById("no-f2f");
-
+const f2fRadio = document.getElementById("f2f_label");
+const noF2fRadio = document.getElementById("no-f2f_label");
 // 거래장소 select 요소 가져오기
 const locationSelect = document.getElementById("location");
 
@@ -9,10 +8,14 @@ document.addEventListener("DOMContentLoaded", function () {
   // 대면 라디오 버튼을 클릭할 때 거래장소 select를 활성화
   f2fRadio.addEventListener("click", function () {
     locationSelect.disabled = false;
+    f2fRadio.childNodes[1].style.display = "block";
+    noF2fRadio.childNodes[1].style.display = "none";
   });
 
   // 비대면 라디오 버튼을 클릭할 때 거래장소 select를 비활성화
   noF2fRadio.addEventListener("click", function () {
     locationSelect.disabled = true;
+    noF2fRadio.childNodes[1].style.display = "block";
+    f2fRadio.childNodes[1].style.display = "none";
   });
 });
