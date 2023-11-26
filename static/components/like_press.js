963e07930925f413c1aee75d275d3602dd2b6711
rnrgll <shuajjjjj@naver.com>
2023-11-27 03:01:28 +0900

    상품 전체 조회 - 찜하기 부분 링크 이동 막음(찜하기 클릭시 상품 상세로 이동하기 않도록)

--- static/components/like_press.js
+++ static/components/like_press.js
@@ -4,9 +4,10 @@ const likeElements = document.querySelectorAll(".item_box1_like");
 // 모든 요소에 대해 클릭 이벤트 리스너를 추가
 likeElements.forEach(function (likeElement) {
   const bookmarkIcon = likeElement.querySelector(".fa-bookmark");
-  const likeCounter = likeElement.querySelector(".item_like_num");
 
-  bookmarkIcon.addEventListener("click", function () {
+  const likeCounter = likeElement.querySelector(".item_like_num");
+  const bookmarkBox = likeElement.querySelector(".like_icon_box");
+  bookmarkBox.addEventListener("click", function () {
     // 현재 클래스를 확인하고 클래스를 변경
     if (bookmarkIcon.classList.contains("fa-regular")) {
       bookmarkIcon.classList.remove("fa-regular");
@@ -19,5 +20,16 @@ likeElements.forEach(function (likeElement) {
       bookmarkIcon.style.color = "#606060";
       likeCounter.textContent = parseInt(likeCounter.textContent) - 1; // 값 감소
     }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const jjimLinks = document.querySelectorAll(".item_box1_like");
+
+  jjimLinks.forEach(function (jjimLink) {
+    jjimLink.addEventListener("click", function (event) {
+      event.preventDefault(); // 기본 동작 막기
+      // 여기에 필요한 로직 추가
+    });
   });
 });
