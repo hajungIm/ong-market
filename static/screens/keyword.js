9ab7ad6d08e6e5349a798cfb8dfa0b73eaf47f2f
MijinKim99 <108381780+Mijin-Ewha@users.noreply.github.com>
2023-11-26 18:47:31 +0900

    keyword 수정중

--- static/screens/keyword.js
+++ static/screens/keyword.js
@@ -7,3 +7,38 @@ var number = parseInt(text.match(/\d+/)[0], 10);
 
 // 결과를 출력합니다.
 console.log(number);
+
+var reviewItems = [
+    document.getElementById("time"),
+    document.getElementById("detail"),
+    document.getElementById("resonable"),
+    document.getElementById("kind"),
+    document.getElementById("condition"),
+    document.getElementById("response"),
+    document.getElementById("give"),
+    document.getElementById("come")
+  ];
+
+var reviewClass = [
+    document.getElementById("green green_1"),
+    document.getElementById("green green_2"),
+    document.getElementById("green green_3"),
+    document.getElementById("green green_4"),
+    document.getElementById("green green_5"),
+    document.getElementById("green green_6"),
+    document.getElementById("green green_7"),
+    document.getElementById("green green_8")
+];
+
+function setOpacity(elementId, opacityValue, elementclass) {
+    var element = document.getElementById(elementId);
+    var opacityValue = element / 10;
+    if (element) {
+      elementclass.style.opacity = opacityValue;
+    }
+  }
+
+  for(var i = 0; i < 8; i++){
+    setOpacity(reviewItems[i], reviewItems[i]/number, reviewClass[i]);
+    reviewClass[i].style.width = reviewItems[i]/number;
+  }
